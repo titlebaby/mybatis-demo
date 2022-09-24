b69deabc42f6f2218665b6255c304e9cacef0e0e
yumiaomiao17040177395 <17040177398@163.com>
2022-09-24 23:36:56 +0800

    动态sql2

--- mybatis-08/src/main/java/com/ling/dao/BlogMapper.java
+++ mybatis-08/src/main/java/com/ling/dao/BlogMapper.java
@@ -10,4 +10,10 @@ public interface BlogMapper {
      int  addBlog(Blog blog);
 
      List<Blog> queryBlogIf(Map map);
+
+     List<Blog> queryBlogChoose(Map map);
+     List<Blog> queryBlogChoose2(Map map);
+     // if eles
+
+     int updateBlog(Map map);
 }
--- mybatis-08/src/test/java/MyTest.java
+++ mybatis-08/src/test/java/MyTest.java
@@ -8,8 +8,41 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import org.apache.log4j.Logger;
+
 
 public class MyTest {
+
+    static Logger logger = Logger.getLogger(MyTest.class);
+
+
+    @Test
+    public void upadte(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        HashMap map = new HashMap<>();
+        map.put("title","java so easy");
+        map.put("author","lll");
+        map.put("id","94ad43ba53464fabafcf39bbf01316c1");
+        mapper.updateBlog(map);
+
+        sqlSession.close();
+    }
+    @Test
+    public void queryBlogChoose2(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        HashMap map = new HashMap<>();
+        map.put("title","mybatis如此简单11");
+        map.put("author","llll");
+        map.put("views",99);
+        List<Blog> blogs = mapper.queryBlogChoose2(map);
+        for (Blog blog : blogs) {
+            System.out.println(blog);
+        }
+        sqlSession.close();
+
+    }
     @Test
     public void addTest(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -39,6 +72,21 @@ public class MyTest {
         map.put("title","mybatis如此简单11");
         map.put("author","lll");
         List<Blog> blogs = mapper.queryBlogIf(map);
+        for (Blog blog : blogs) {
+            System.out.println(blog);
+        }
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void addTestQuery2(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        HashMap map = new HashMap<>();
+        map.put("title","mybatis如此简单11");
+        map.put("author","llll");
+        List<Blog> blogs = mapper.queryBlogChoose(map);
         for (Blog blog : blogs) {
             System.out.println(blog);
         }
