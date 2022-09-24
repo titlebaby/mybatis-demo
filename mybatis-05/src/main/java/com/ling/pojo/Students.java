b1c2e528a70d9c481210bf9ceb2aff7e0179dab2
yumiaomiao17040177395 <17040177398@163.com>
2022-09-24 11:12:54 +0800

    Lombok的使用

---
+++ mybatis-05/src/main/java/com/ling/pojo/Students.java
@@ -0,0 +1,14 @@
+package com.ling.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Students {
+    private int id;
+    private String name;
+    private String age;
+}
